9ede641df50edfb5fd533785efc3236a2cd520db
Himanshu Jain <hjain@Egens-MBP.attlocal.net>
2018-02-15 08:21:14 -0600

    add allergies, routing and menu details

---
+++ app/screens/Allergies.js
@@ -0,0 +1,31 @@
+/* eslint-disable react/prefer-stateless-function */
+
+import React, { Component } from 'react';
+import { FlatList } from 'react-native';
+import Record from '../components/Record';
+import { allergies } from '../config/data';
+import colors from "../config/colors"
+
+class Me extends Component {
+    constructor(props) {
+        super(props)
+        this.state = { isChecked: false };
+    }
+  render() {
+    return (
+
+        <FlatList
+            style={{ backgroundColor: colors.background }}
+            data={allergies}
+            renderItem={({item}) =>
+                <Record item={item} />
+            }
+            keyExtractor={(item) => item.id}
+        ></FlatList>
+
+
+    );
+  }
+}
+
+export default Me;
